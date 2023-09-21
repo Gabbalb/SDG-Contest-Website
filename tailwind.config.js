30fbc339f66b9cce063912cb0527727dd62a44bc
gabbalb <balbiani.gabriele@gmail.com>
2023-09-21 17:46:17 +0200

    Draft#1 homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,49 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: [
+    './src/pages/*.{html,js,svelte,ts}',
+    './node_modules/@cozemble/data-paginated-editor/**/*.{html,js,svelte,ts}'
+  ],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+    ],
+  }
 }
+
+
 
